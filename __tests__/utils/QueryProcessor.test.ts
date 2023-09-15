e1832148eae9d64d7de4c24110c2afa77bab81e6
Simran Bedi <simranbedi@macbook-pro-424.wifi.local.cmu.edu>
2023-09-15 13:36:40 -0400

    added andrew id functionality

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,11 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+    test('should return andrew Id ', () => {
+        const query = "What is my Andrew ID";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+           "skbedi"
+          ));
+    });
 });
